733a0fc951f3ef35e7016b967c582ed850dc3d6c
Vladimir <fominsky_v@live.com>
2022-04-13 02:57:32 +0500

    Homewrok_5

---
+++ src/main/java/ru/gb/dao/HibernateProductDao.java
@@ -0,0 +1,52 @@
+package ru.gb.dao;
+
+import lombok.RequiredArgsConstructor;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import ru.gb.entity.Product;
+
+import java.util.Collections;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class HibernateProductDao implements ProductDao {
+
+    private final SessionFactory sessionFactory;
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<Product> findAll() {
+        return Collections.unmodifiableList(sessionFactory.getCurrentSession().createQuery("from \"public\".\"PRODUCT\" m").list());
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public String findTitleById(Long id) {
+        return (String) sessionFactory.getCurrentSession().getNamedQuery("Product.findTitleById")
+                .setParameter("id", id).uniqueResult();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Product findById(Long id) {
+        return (Product) sessionFactory.getCurrentSession().getNamedQuery("Product.findById")
+                .setParameter("id", id).uniqueResult();
+    }
+
+    @Override
+    public void insert(Product product) {
+
+    }
+
+    @Override
+    public void update(Product product) {
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+}
--- src/main/java/ru/gb/dao/ProductDao.java
+++ src/main/java/ru/gb/dao/ProductDao.java
@@ -1,4 +1,13 @@
 package ru.gb.dao;
 
+import ru.gb.entity.Manufacturer;
+import ru.gb.entity.Product;
+
 public interface ProductDao {
+    Iterable<Product> findAll();
+    String findTitleById(Long id);
+    Product findById(Long id);
+    void insert(Product product);
+    void update(Product product);
+    void deleteById(Long id);
 }
